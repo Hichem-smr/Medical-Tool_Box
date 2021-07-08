181e3b3f32c2aa96913ba7ade5d9f40c598001a8
hichemhero <44125330+hichemhero@users.noreply.github.com>
2021-07-08 20:07:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+########################################################################
+########################################################################
+
+########################################################################
+## IMPORTS
+########################################################################
+import os
+import sys
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from ui_interface import *
+from Custom_Widgets.Widgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        loadJsonStyle(self, self.ui)
+        self.ui.menu_widget.collapseMenu()
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
